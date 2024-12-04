4d114205833f1a178e99a8d15bc7b45f1652433a
Collins Janarthana <collinsjanarthana@gmail.com>
2024-12-04 21:18:15 +0530

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+function App(){
+  
+  return (
+    <Router>
+      {""}
+      <div className='app-container'>
+        <Routes>
+          <Route path='/' element={<h1>Dashboard</h1>}/>
+          <Route path='/auth' element={<h1>Sign in</h1>}/>
+      
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
